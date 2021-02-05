59b60c2ec4e64bb53b43bdc3dff0b8f355bdd0f4
Andy Bryant <a.mcdermid.bryant@gmail.com>
2021-02-04 21:19:01 -0500

    added title and content

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import './App.css';
 function App() {
   return (
     <div className="background-img">
+      <div className="content">
+        <form action="" id='title-form'>
+          <label htmlFor="title-input">Enter Text</label>
+          <input type="text" id='title-input'/>
+        </form>
+      </div>
+      <footer><span className='page-title'>Breaking Bad Title Generator - © 2021 Andy Bryant</span></footer>
     </div>
   );
 }
