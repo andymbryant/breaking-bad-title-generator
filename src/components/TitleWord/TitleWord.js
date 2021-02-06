a9acb9319cbac5963b610695da025314d676ff8b
Andy Bryant <a.mcdermid.bryant@gmail.com>
2021-02-06 14:40:07 -0500

    added prop types to title word

--- src/components/TitleWord/TitleWord.js
+++ src/components/TitleWord/TitleWord.js
@@ -1,6 +1,6 @@
-
 import Element from '../Element/Element'
 import Char from '../Char/Char'
+import PropTypes from 'prop-types'
 
 // A title word is made up of either element or character components
 function TitleWord({titleUnits}) {
@@ -9,6 +9,14 @@ function TitleWord({titleUnits}) {
       {titleUnits.map(unit => unit.type === 'element' ? <Element key={unit.id} {...unit.data}/> : <Char key={unit.id} {...unit.data}/>)}
     </div>
   )
+}
+
+TitleWord.propTypes = {
+  titleUnits: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+    data: PropTypes.object.isRequired
+  }))
 }
 
 export default TitleWord
