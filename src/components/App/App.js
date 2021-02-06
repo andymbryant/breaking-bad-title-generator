aa1476a70c69060fd974172534c269888cc4f75f
Andy Bryant <a.mcdermid.bryant@gmail.com>
2021-02-06 09:32:40 -0500

    rearranged component file structure and naming

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import { useReducer } from "react";
 import TitleDisplay from '../TitleDisplay/TitleDisplay'
-import TitleForm from '../TitleForm/TitleForm'
+import TextInput from '../TextInput/TextInput'
 import Footer from '../Footer/Footer'
 import {TitleContext} from '../../contexts/TitleContext'
 import {titleReducer, defaultTitleValue} from '../../reducers/titleReducer'
@@ -12,7 +12,7 @@ function App() {
     <TitleContext.Provider value={{title, dispatch}}>
       <div className="background-img">
         <div className="title-content">
-          <TitleForm/>
+          <TextInput/>
           <TitleDisplay/>
         </div>
         <Footer />
--- src/components/TextInput/TextInput.js
+++ src/components/TextInput/TextInput.js
@@ -1,23 +1,24 @@
 import {useContext, useRef} from 'react'
-import './TitleForm.css'
+import './TextInput.css'
 import {TitleContext} from '../../contexts/TitleContext'
 
-function TitleInput() {
+function TextInput() {
   const inputEl = useRef(null);
   // eslint-disable-next-line no-unused-vars
   const {title, dispatch} = useContext(TitleContext)
+  // const handleSubmit = (e) => e.preventDefault()
   const handleSetTitle = (e) => dispatch({type: 'SET_TITLE', payload: e.target.value})
   const handleClearTitle = (e) => {
     e.preventDefault()
     dispatch({type: 'CLEAR_TITLE'})
   }
   return (
-    <form id='title-form'>
+    <div className='text-input-ctr'>
       <label htmlFor="title-input">Enter Text</label>
       <input ref={inputEl} value={title} onChange={handleSetTitle} type="text" id='title-input'/>
       <button onClick={handleClearTitle}>Clear</button>
-    </form>
+    </div>
   )
 }
 
-export default TitleInput
+export default TextInput
