618b12dba5213df914912c1668ad2d56b3573ab2
Andy Bryant <a.mcdermid.bryant@gmail.com>
2021-02-06 16:42:00 -0500

    added material input and button

--- src/components/TextInput/TextInput.js
+++ src/components/TextInput/TextInput.js
@@ -1,9 +1,16 @@
-import {useContext, useRef} from 'react'
+import {useContext} from 'react'
 import './TextInput.css'
 import {TitleContext} from '../../contexts/TitleContext'
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 function TextInput() {
-  const inputEl = useRef(null);
+  const buttonStyle = {
+    height: '100%'
+  }
+  const textInputStyle = {
+    color: 'green'
+  }
   // eslint-disable-next-line no-unused-vars
   const {title, dispatch} = useContext(TitleContext)
   const handleSetTitle = (e) => dispatch({type: 'SET_TITLE', payload: e.target.value})
@@ -13,9 +20,24 @@ function TextInput() {
   }
   return (
     <div className='text-input-ctr'>
-      <label className='text-input-label' htmlFor="text-input">Enter Text</label>
-      <input data-cy='text-input' ref={inputEl} value={title} onChange={handleSetTitle} type="text" id='text-input'/>
-      <button data-cy='clear-btn' onClick={handleClearTitle}>Clear</button>
+      {/* <input data-cy='text-input' value={title} onChange={handleSetTitle} type="text" id='text-input'/> */}
+      <TextField
+        variant='outlined'
+        label='Enter Text'
+        data-cy='txt-input'
+        style={textInputStyle}
+        value={title}
+        id='text-input'
+        fullWidth={true}
+        onChange={handleSetTitle}
+      />
+      <Button
+        color='red'
+        data-cy='clear-btn'
+        variant='contained'
+        style={buttonStyle}
+        onClick={handleClearTitle}
+      >Clear</Button>
     </div>
   )
 }
