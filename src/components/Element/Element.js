378a2a33c6ebb1dfb08ca0a74470e9adf92150ea
Andy Bryant <a.mcdermid.bryant@gmail.com>
2021-02-06 09:19:24 -0500

    removed unecessary div in element

--- src/components/Element/Element.js
+++ src/components/Element/Element.js
@@ -1,10 +1,6 @@
 import './Element.css'
 function Element({symbol}) {
-  return (
-    <div className="element-ctr">
-      <div>{symbol}</div>
-    </div>
-  )
+  return <div className="element-ctr">{symbol}</div>
 }
 
 export default Element
