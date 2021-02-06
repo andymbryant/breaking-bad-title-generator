f0ec66109474bb62121d1925d9f220da8f1cab50
Andy Bryant <a.mcdermid.bryant@gmail.com>
2021-02-06 14:43:09 -0500

    added prop types to element and char

--- src/components/Char/Char.js
+++ src/components/Char/Char.js
@@ -1,11 +1,12 @@
+import PropTypes from 'prop-types'
 import './Char.css'
-function Char({str}) {
-  return (
-    <div className='char-ctr'>
-      {str}
-    </div>
-  )
 
+function Char({str}) {
+  return <div className='char-ctr'>{str}</div>
+}
+
+Char.propTypes = {
+  str: PropTypes.string.isRequired
 }
 
 export default Char
--- src/components/Element/Element.js
+++ src/components/Element/Element.js
@@ -1,6 +1,12 @@
+import PropTypes from 'prop-types'
 import './Element.css'
+
 function Element({symbol}) {
   return <div className="element-ctr">{symbol}</div>
+}
+
+Element.propTypes = {
+  symbol: PropTypes.string.isRequired
 }
 
 export default Element
