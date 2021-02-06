3a5ba65cd8ab46e6c6a65b033ef68e48960a09c9
Andy Bryant <a.mcdermid.bryant@gmail.com>
2021-02-06 14:56:16 -0500

    styled footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,15 @@
+import './Footer.css'
+
 function Footer() {
   return (
     <footer>
-      <span className='page-title'>Breaking Bad Title Generator - © 2021 Andy Bryant</span>
+      <span className='footer-ctr'>
+        <span className='footer-title'>Breaking Bad Title Generator</span>
+        <span> • </span>
+        <span className='copyright'>© 2021 Andy Bryant</span>
+        <span> • </span>
+        <a className='code-link' href="https://github.com/andymbryant/breaking-bad-title-generator" target='_blank' rel="noreferrer">Code</a>
+      </span>
     </footer>
   )
 }
